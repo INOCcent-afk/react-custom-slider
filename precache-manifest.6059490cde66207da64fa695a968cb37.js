3efa1d484d30c5a76a5024650571f686581f3ac0
INOCcent-afk <michael828inoc@gmail.com>
2021-01-29 01:01:13 +0800

    Updates

---
+++ precache-manifest.6059490cde66207da64fa695a968cb37.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91105b9aeb2d2115aafdce7ffcd7cf5a",
+    "url": "/react-custom-slider/index.html"
+  },
+  {
+    "revision": "8050eddbec746786732c",
+    "url": "/react-custom-slider/static/css/2.7fad5d03.chunk.css"
+  },
+  {
+    "revision": "4ca063aa5f9cb3eb4481",
+    "url": "/react-custom-slider/static/css/main.090c3176.chunk.css"
+  },
+  {
+    "revision": "8050eddbec746786732c",
+    "url": "/react-custom-slider/static/js/2.f277c295.chunk.js"
+  },
+  {
+    "revision": "4ca063aa5f9cb3eb4481",
+    "url": "/react-custom-slider/static/js/main.2f90ff40.chunk.js"
+  },
+  {
+    "revision": "21ade697b86d26daff5d",
+    "url": "/react-custom-slider/static/js/runtime~main.7237dde1.js"
+  },
+  {
+    "revision": "4cd3b2b446ec7fe241401deca3057332",
+    "url": "/react-custom-slider/static/media/arrow-left.4cd3b2b4.svg"
+  },
+  {
+    "revision": "116a70ada1a49d26fac6b51742f49e34",
+    "url": "/react-custom-slider/static/media/arrow-right.116a70ad.svg"
+  },
+  {
+    "revision": "00fcc588f8471e3562d62edf67c83daa",
+    "url": "/react-custom-slider/static/media/image.00fcc588.jpg"
+  },
+  {
+    "revision": "b500dfd61c58377c0c72caae642e783a",
+    "url": "/react-custom-slider/static/media/image2.b500dfd6.jpg"
+  },
+  {
+    "revision": "6dfebea3407a5de927f0aef8e126a401",
+    "url": "/react-custom-slider/static/media/image3.6dfebea3.jpg"
+  }
+]);
